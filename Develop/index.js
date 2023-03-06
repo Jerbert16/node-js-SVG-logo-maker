7173d4e450ca65745c50d96bf8e5a8f8d5ad849b
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-03-05 17:39:34 -0700

    generate SVG file complete/reformatted code with prettier

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,10 @@
 //installed inquirer 
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { choices } = require("yargs");
 //prompt questions
-const theQuestions = [
+inquirer
+.prompt ([
   {
     type: "input",
     name: "letters",
@@ -28,16 +30,44 @@ const theQuestions = [
     name: "shapeColor",
     message: "What color do you want that shape?",
   },
-];
+])
+.then((responses) => {
+if (responses.choices = 1) {
+const generateSVG = (
+`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-function init() {
-    inquirer.prompt(theQuestions).then((data) => {
-      // const svgContent = generateSvg(data);
-      //write the README file
-    //   fs.writeFile("README.md", readMeContent, (err) =>
-    //     err ? console.log(err) : console.log(data)
-    //   );
-    });
-  }
-  // call init function
-  init();
+<circle cx="150" cy="100" r="80" fill="green" />
+
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">${responses.letters}</text>
+
+</svg>`
+)
+fs.writeFile("newSVG.svg", generateSVG, (err) =>
+err ? console.log(err) : console.log(responses))
+}})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function init() {
+//     inquirer.prompt(theQuestions).then((data) => {
+//       // const svgContent = generateSvg(data);
+//       //write the README file
+//     //   fs.writeFile("README.md", readMeContent, (err) =>
+//     //     err ? console.log(err) : console.log(data)
+//     //   );
+//     });
+//   }
+//   // call init function
+//   init();
